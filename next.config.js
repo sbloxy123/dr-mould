66c8bbec16422bba9780ec806245f073715abc40
Stuart Bloxham <s.bloxham@hotmail.co.uk>
2023-12-11 22:23:18 +0000

    contact

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+module.exports = {
+  env: {
+    customKey: "my-value",
+  },
+};
