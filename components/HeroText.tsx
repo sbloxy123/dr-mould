ca3f37289f1cfd7df476d2b53513f7806f31f98c
Stuart Bloxham <s.bloxham@hotmail.co.uk>
2024-09-10 22:08:27 +0100

    added subtitle to hero

--- components/HeroText.tsx
+++ components/HeroText.tsx
@@ -5,15 +5,17 @@ const HeroText = () => {
   return (
     <div className="relative flex h-screen content-container">
       <div className="w-[100vw] justify-center text-center md:text-left md:w-[60vw] my-auto">
-        <h1 className="text-5xl pb-12 font-patua text-theme_indigo-900 uppercase tracking-wide">
+        <h1 className="text-5xl pb-3 font-patua text-theme_indigo-900 uppercase tracking-wide">
           Dr Mould: Your Trusted Mould Treatment Solution
         </h1>
+        <h3 className="text-3xl pb-8 tracking-wide font-normal text-theme_indigo-900">
+          Reclaim your space, breathe easy, and restore beauty.
+        </h3>
         <p className="pl-0 pb-12 p-2 w-full md:w-[80%] tracking-wide text-lg text-theme_indigo-900">
-          Reclaim your space, breathe easy, and restore beauty. Our meticulous
-          approach ensures effective mould removal, leaving your home looking
-          fresh and revitalised. Trust Dr Mould for professional and thorough
-          mould treatment services in the Hertfordshire, Essex and Cambridgshire
-          areas.
+          Our meticulous approach ensures effective mould removal, leaving your
+          home looking fresh and revitalised. Trust Dr Mould for professional
+          and thorough mould treatment services in the Hertfordshire, Essex and
+          Cambridgshire areas.
         </p>
         <Link
           href="/contact"
